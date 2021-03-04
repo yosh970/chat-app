48356a59b1270cee658e4f3abbe1051983012b7e
Josselyn Mas <josh.jmas@gmail.com>
2021-03-04 20:17:43 +0100

    quest 02.2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import Contact from "./component/Contact";
 function App() {
   return (
     <div className="App">
-      <Contact/>
+      <Contact
+      name="Letitia Howell"
+      avatar="https://randomuser.me/api/portraits/women/41.jpg"
+      online = {true}
+      />
+      <Contact
+      name="Penny Wood"
+      avatar="https://randomuser.me/api/portraits/women/77.jpg"
+      online = {false}
+      />
+      <Contact
+      name="William Gibson"
+      avatar="https://randomuser.me/api/portraits/men/25.jpg"
+      online = {true}
+      />
     </div>
   );
 }
--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import './Contact.css'
+import './Contact.css';
+import PropTypes from 'prop-types';
+class Contact extends React.Component {
+  render() {
+    return(
+      <figure className="Contact">
+        <img className="avatar" src={this.props.avatar} alt="avatar"/>
+        <figcaption>
+          <p className="name">{this.props.name}</p>
+          <div className="status">
+            <span className={this.props.online ? "status-online": "status-offline"}/>
+            <p className="status-text">{this.props.online ? "online": "offline"}</p>
+          </div>
+        </figcaption>
+      </figure>
+    )
+  }
+}
 
-const name = "Letitia Howell";
-const avatar = "https://randomuser.me/api/portraits/women/41.jpg";
-const online = true;
-
-function Contact() {
-  return(
-    <figure className="Contact">
-      <img className="avatar" src={avatar}/>
-      <figcaption>
-        <p className="name">{name}</p>
-        <div className="status">
-          <span className={online ? "status-online": "status-offline"}/>
-          <p className="status-text">{online ? "online": "offline"}</p>
-        </div>
-      </figcaption>
-    </figure>
-  )
-};
+Contact.propTypes= {
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  online: PropTypes.bool.isRequired
+}
 
 export default Contact;
